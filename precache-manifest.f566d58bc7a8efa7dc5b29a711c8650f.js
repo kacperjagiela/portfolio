5eb2e3e1e08fe6cd4ed609ede60a8e0756f8de12
kacperjagiela <jagielakacper80@gmail.com>
2019-08-20 21:32:48 +0200

    Updates

---
+++ precache-manifest.f566d58bc7a8efa7dc5b29a711c8650f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1db2687f277d841d9c8213772da48f7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "100195d88569e6469388",
+    "url": "/portfolio/static/css/main.7ca295cc.chunk.css"
+  },
+  {
+    "revision": "f1f936b0ea51a59fa828",
+    "url": "/portfolio/static/js/2.8951cbb8.chunk.js"
+  },
+  {
+    "revision": "100195d88569e6469388",
+    "url": "/portfolio/static/js/main.df438512.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2c510c6cf9f24de0857557035fdf183f",
+    "url": "/portfolio/static/media/cv.2c510c6c.zip"
+  },
+  {
+    "revision": "b36ec80896cf2b41d61077b31a26ebe0",
+    "url": "/portfolio/static/media/project1.b36ec808.png"
+  },
+  {
+    "revision": "2ad9a2f41614dbd3dbb2e4d30adc4c41",
+    "url": "/portfolio/static/media/project2.2ad9a2f4.png"
+  },
+  {
+    "revision": "4fc847b18f191bd9dacb7caf54c57714",
+    "url": "/portfolio/static/media/project3.4fc847b1.png"
+  }
+]);
